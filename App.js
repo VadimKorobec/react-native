fe0eefb723f772f75e54b3c5798491f05ff78e76
VadimKorobec <_s_e_t__@ukr.net>
2023-06-07 18:33:09 +0300

    Update App.js

--- App.js
+++ App.js
@@ -28,8 +28,6 @@ const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
     height: 50,
-    backgroundColor: "#F6F6F6",
-    borderColor: "#E8E8E8",
     borderRadius: 8,
     marginHorizontal: 16,
   },
