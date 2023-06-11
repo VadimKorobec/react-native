b227193987e957267aa7c8e62371b5f52b6ee650
VadimKorobec <_s_e_t__@ukr.net>
2023-06-11 17:23:55 +0300

    Update Screen.js

--- Screens/PostsScreen/Screen.js
+++ Screens/PostsScreen/Screen.js
@@ -1,36 +1,54 @@
 import React, { useState } from "react";
 import {
   StyleSheet,
+  Text,
   View,
-  TextInput,
-  KeyboardAvoidingView, // новий імпорт
-  Platform, // новий імпорт
   ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
 } from "react-native";
 
-export default function Screen() {
-  const [text, setText] = useState("");
+export default function App() {
+  console.log(Platform.OS);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
   return (
     <View style={styles.container}>
-      <KeyboardAvoidingView // визначаємо ОС та налаштовуємо поведінку клавіатури
-        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      <ImageBackground
+        style={styles.image}
+        source={require("../../assets/images/photo-bg.jpg")}
       >
-        <ImageBackground
-          style={styles.image}
-          source={require("../../assets/images/photo-bg.jpg")}
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
         >
-          <TextInput
-            placeholder="Type text"
-            value={text}
-            onChangeText={setText}
-          />
-          <TextInput
-            placeholder="Type text"
-            value={text}
-            onChangeText={setText}
-          />
-        </ImageBackground>
-      </KeyboardAvoidingView>
+          <View
+            style={{ ...styles.form, marginBottom: isShowKeyboard ? 20 : 100 }}
+          >
+            <View>
+              <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>PASSWORD</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                secureTextEntry={true}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+            </View>
+            <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+              <Text style={styles.btnTitle}>SIGN IN</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </ImageBackground>
     </View>
   );
 }
@@ -39,13 +57,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-end",
   },
   image: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "flex-end",
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#f0f8ff",
+    height: 40,
+    borderRadius: 6,
+
+    color: "#f0f8ff",
+  },
+  form: {
+    marginHorizontal: 40,
+  },
+  inputTitle: {
+    color: "#f0f8ff",
+    marginBottom: 10,
+    fontSize: 18,
+  },
+  btn: {
+    borderRadius: 6,
+    borderWidth: 1,
+    height: 40,
+    marginTop: 40,
+    justifyContent: "center",
     alignItems: "center",
+    marginHorizontal: 20,
+    ...Platform.select({
+      ios: {
+        backgroundColor: "transparent",
+        borderColor: "#f0f8ff",
+      },
+      android: {
+        backgroundColor: "#4169e1",
+        borderColor: "transparent",
+      },
+    }),
+  },
+  btnTitle: {
+    color: Platform.OS === "ios" ? "#4169e1" : "#f0f8ff",
+    fontSize: 18,
   },
 });
